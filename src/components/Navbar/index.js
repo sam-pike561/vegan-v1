a6b9f156b7713fb9541b241adb18bf3a67347b17
Samuel Pike <samuelp1dev@gmail.com>
2022-09-07 09:58:25 -0700

    refactored restaurant template v1

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Nav, NavLink, NavIcon, Bars } from './NavbarElements';
+
+const Navbar = ({ toggle }) => {
+  return (
+    <>
+      <Nav>
+        <NavLink to='/'>FIHOP</NavLink>
+        <NavIcon onClick={toggle}>
+          <p>Menu</p>
+          <Bars />
+        </NavIcon>
+        <NavIcon onClick={toggle}>
+          <p>Menu</p>
+          <Bars />
+        </NavIcon>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Products/data.js
@@ -0,0 +1,75 @@
+import product1 from '../../images/product-1.jpg';
+import product2 from '../../images/product-2.jpg';
+import product3 from '../../images/product-3.jpg';
+import sweet1 from '../../images/sweet3.jpg';
+import sweet2 from '../../images/sweet-2.jpg';
+import sweet3 from '../../images/sweet-3.jpg';
+import ImgBg from '../../images/ultimate-veggie-burger.webp';
+import ImgMsh from '../../images/Smashroom.webp';
+import ImgChkn from '../../images/fried-chicken-sandwiches-with-slaw-and-spicy-mayo.webp';
+import ImgBananaCakes from '../../images/Vegan_Banana_Oatmeal_Pancake_16x9.webp';
+import ImgHalvah from '../../images/salted-chocolate-halva.webp';
+
+
+
+
+
+export const productData = [
+  {
+    img: ImgBg,
+    alt: 'Burger',
+    name: 'Smash Burger',
+    desc:
+      'two smash patties (V), lettuce, pickles, roma tomatoes, american cheese (V), and grilled sesame seed bun',
+    price: '$19.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: ImgMsh,
+    alt: 'Smashroom',
+    name: 'Day Tripper',
+    desc:
+      '  aioli (V), fungi misti, fresh mozzarella (V), tomatoes, Calabrian peppers, and crispy italian bread',
+    price: '$16.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: ImgChkn,
+    alt: 'ChickenSandwich',
+    name: 'Crunchy Chicken Sandwich',
+    desc:
+      'coleslaw, banana peppers, vegan cajun aioli, impossible chicken on ciabbata roll',
+    price: '$14.99',
+    button: 'Add to Cart'
+  }
+];
+
+export const productDataTwo = [
+  {
+    img: sweet2,
+    alt: 'Donuts',
+    name: 'Doughlicious',
+    desc:
+      'vegan cake and yeast donuts with fresh, seasonal flavors',
+    price: '$9.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: ImgBananaCakes,
+    alt: 'Banana Pancakes',
+    name: 'Banana-Oat Pancakes',
+    desc:
+      'With chocolate chips and topped with lemon zest,  greek yogurt and maple syrup or honey',
+    price: '$12.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: ImgHalvah,
+    alt: 'halva',
+    name: 'Chocolate-Covered Halva',
+    desc:
+      'a soft, fudgelike candy made out of sesame paste coated in chocolate and coarse sea salt',
+    price: '$9.99',
+    button: 'Add to Cart'
+  }
+];
